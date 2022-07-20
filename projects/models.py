d9e61ec5bd9730ae4ce53858bbe7346b9ecfd725
vangola91 <aliksandrmax@yandex.ru>
2022-07-20 09:58:04 +0500

    DYI22

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 import uuid
+from users.models import Profile
 
 
 class Project(models.Model):
-    # owner =
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=160)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
