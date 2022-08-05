f368d9c486ff1323250ce20e8b1a718cbeaeac77
vangola91 <aliksandrmax@yandex.ru>
2022-08-05 11:25:21 +0500

    Email activ

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
+
+from django.core.mail import send_mail
+from django.conf import settings
+
 # @receiver(post_save, sender=Profile)
 
 
@@ -15,6 +19,18 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
@@ -27,8 +43,11 @@ def updateUser(sender, instance, created, **kwargs):
 
 
 def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
 
 
 post_save.connect(createProfile, sender=User)
